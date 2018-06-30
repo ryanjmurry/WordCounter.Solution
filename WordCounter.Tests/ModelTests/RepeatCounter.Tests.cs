6971e4919d0b71881c6c038a6880e5ee0606e357
Ryan Murry <ryanjmurry@gmail.com>
2018-06-30 03:54:49 -0700

    add final methods and tests to determine the number of matches

--- WordCounter.Tests/ModelTests/RepeatCounter.Tests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounter.Tests.cs
@@ -90,7 +90,7 @@ namespace WordCounter.Tests
         public void ValidateTargetWord_ValidatesDashes_True()
         {
             //Arrange
-            RepeatCounter newRepeatCounter = new RepeatCounter("in-class");
+            RepeatCounter newRepeatCounter = new RepeatCounter("in-line");
 
             //Act
             bool result = newRepeatCounter.ValidateTargetWord();
@@ -224,7 +224,7 @@ namespace WordCounter.Tests
         }
 
         [TestMethod]
-        public void CleanList_CleanSearchListOfAnyBookendPunctuation_List()
+        public void CleanList_CleanSingleElementSearchListOfAnyBookendPunctuation_List()
         {
             //Arrange
             RepeatCounter newRepeatCounter = new RepeatCounter("", "'can'");
@@ -237,209 +237,85 @@ namespace WordCounter.Tests
             //Assert
             CollectionAssert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void CleanList_CleanSearchListOfAnyBookendPunctuation_List()
+        {
+            //Arrange
+            RepeatCounter newRepeatCounter = new RepeatCounter("", "i 'can' swim!");
+            List<string> expected = new List<string>() { "i", "can", "swim" };
+
+            //Act
+            newRepeatCounter.CleanList();
+            List<string> actual = newRepeatCounter.GetSearchList();
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Matches_DeterminesNumberOfMatches1_Int()
+        {
+            //Arrange
+            RepeatCounter newRepeatCounter = new RepeatCounter("can", "i can swim");
+
+            //Act
+            int result = newRepeatCounter.Matches();
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Matches_DeterminesNumberOfMatches2_Int()
+        {
+            //Arrange
+            RepeatCounter newRepeatCounter = new RepeatCounter("can", "I CAN SWIM");
+
+            //Act
+            int result = newRepeatCounter.Matches();
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Matches_DeterminesNumberOfMatches3_Int()
+        {
+            //Arrange
+            RepeatCounter newRepeatCounter = new RepeatCounter("can", "I 'can' swim! Can you?");
+
+            //Act
+            int result = newRepeatCounter.Matches();
+
+            //Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Matches_DeterminesNumberOfMatches4_Int()
+        {
+            //Arrange
+            RepeatCounter newRepeatCounter = new RepeatCounter("in-line", "I like to in-line rollerskate.");
+
+            //Act
+            int result = newRepeatCounter.Matches();
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Matches_DeterminesNumberOfMatches5_Int()
+        {
+            //Arrange
+            RepeatCounter newRepeatCounter = new RepeatCounter("don't", "Don't go there!");
+
+            //Act
+            int result = newRepeatCounter.Matches();
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
     }
 }
-
-        //Old Tests
-//         [TestMethod]
-//         public void GetSetTargetWord_GetsAndSetsTargetWord_String()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//             newObject.SetTargetWord("can");
-//
-//             //Act
-//             string result = newObject.GetTargetWord();
-//
-//             //Assert
-//             Assert.AreEqual("can", result);
-//         }
-//
-//         [TestMethod]
-//         public void CheckTargetWordForPunctuation_ChecksTargetWordForPunctuation_True()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//             newObject.SetTargetWord("can");
-//
-//             //Act
-//             bool result = newObject.CheckTargetWordForPunctuation();
-//
-//             //Assert
-//             Assert.AreEqual(true, result);
-//         }
-//
-//         [TestMethod]
-//         public void CheckTargetWordForPunctuation_ChecksTargetWordForPunctuation_False()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//             newObject.SetTargetWord("c@n");
-//
-//             //Act
-//             bool result = newObject.CheckTargetWordForPunctuation();
-//
-//             //Assert
-//             Assert.AreEqual(false, result);
-//         }
-//
-//         [TestMethod]
-//         public void SetTargetWord_SetsTargetWordToLowerCase_String()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//             newObject.SetTargetWord("CAN");
-//
-//             //Act
-//             string result = newObject.GetTargetWord();
-//
-//             //Assert
-//             Assert.AreEqual("can", result);
-//         }
-//
-//         [TestMethod]
-//         public void GetSetSearchPhrase_GetsAndSetsSearchPhrase_String()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//             newObject.SetSearchPhrase("i can open the can of tuna.");
-//
-//             //Act
-//             string result = newObject.GetSearchPhrase();
-//
-//             //Assert
-//             Assert.AreEqual("i can open the can of tuna.", result);
-//         }
-//
-//         [TestMethod]
-//         public void SetSearchPhraseToLower_SetsSearchPhraseToLowerCase_String()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//             newObject.SetSearchPhrase("I CAN OPEN THE CAN OF TUNA.");
-//
-//             //Act
-//             newObject.SetSearchPhraseToLower();
-//             string result = newObject.GetSearchPhrase();
-//
-//             //Assert
-//             Assert.AreEqual("i can open the can of tuna.", result);
-//         }
-//
-//         [TestMethod]
-//         public void GetSetSearchList_GetsAndSetsSearchListFromSearchPhrase_List()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//             newObject.SetSearchList("i can open the can of tuna.");
-//
-//             //Act
-//             List<string> expected = new List<string>() { "i", "can", "open", "the", "can", "of", "tuna." };
-//             List<string> result = newObject.GetSearchList();
-//
-//             //Assert
-//             CollectionAssert.AreEqual(expected, result);
-//         }
-//
-//         [TestMethod]
-//         public void SetArrayEnd_SetsIndexAtEndOfArrayToSearchAt_Int()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//
-//             //Act
-//             newObject.SetArrayEnd("'can'");
-//             int result = newObject.GetArrayEnd();
-//
-//             //Assert
-//             Assert.AreEqual(4, result);
-//         }
-//
-//         //------------Buggy Edge Case Tests-----------------//
-//
-//         // [TestMethod]
-//         // public void GetSetCleanSearchList_GetsAndSetsCleanSearchListFromSearchList_List()
-//         // {
-//         //     //Arrange
-//         //     RepeatCounter newObject = new RepeatCounter();
-//         //     newObject.SetSearchList("'can'");
-//         //
-//         //     //Act
-//         //     newObject.SetCleanSearchList();
-//         //     List<string> expected = new List<string>() { "can" };
-//         //     List<string> result = newObject.GetSearchList();
-//         //     foreach(string word in expected)
-//         //     {
-//         //         Console.WriteLine(word);
-//         //     }
-//         //     Console.WriteLine("");
-//         //     foreach(string word in result)
-//         //     {
-//         //         Console.WriteLine(word);
-//         //     }
-//         //     Console.WriteLine(newObject.GetArrayStart());
-//         //     Console.WriteLine(newObject.GetArrayEnd());
-//         //
-//         //
-//         //     CollectionAssert.AreEqual(expected, result);
-//         // }
-//
-//         //------------End of Buggy Edge Case Tests-----------------//
-//
-//         [TestMethod]
-//         public void IncrementMatches_IncrementNumberOfMatches_Int()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//
-//             //Act
-//             newObject.IncrementMatches();
-//             int result = newObject.GetMatches();
-//
-//             //Assert
-//             Assert.AreEqual(1, result);
-//         }
-//
-//         [TestMethod]
-//         public void SetGetMatches_SetAndGetNumberOfMatches_Int()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//             newObject.SetTargetWord("can");
-//             newObject.SetSearchList("I can open a can of tuna.");
-//
-//             //Act
-//             newObject.SetMatches();
-//             int result = newObject.GetMatches();
-//
-//             //Assert
-//             Assert.AreEqual(2, result);
-//         }
-//
-//         [TestMethod]
-//         public void NumberOfMatches_NumberOfMatchesInSearchList_Int()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//
-//             //Act
-//             int result = newObject.NumberOfMatches("CAN", "I CAN OPEN A CAN OF TUNA");
-//
-//             //Assert
-//             Assert.AreEqual(2, result);
-//         }
-//
-//         [TestMethod]
-//         public void NumberOfMatches_NumberOfMatchesInSearchListWithPunctuation_Int()
-//         {
-//             //Arrange
-//             RepeatCounter newObject = new RepeatCounter();
-//
-//             //Act
-//             int result = newObject.NumberOfMatches("CAN", "I 'CAN' OPEN A CAN OF TUNA.");
-//
-//             //Assert
-//             Assert.AreEqual(1, result);
-//         }
-//     }
-// }
--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -27,13 +27,6 @@ namespace WordCounter
             return _targetWord;
         }
 
-        //Should be in main? Asking for user input...
-        // public void SetTargetWord()
-        // {
-        //     Console.WriteLine("Please enter a target word to check for: ");
-        //     _targetWord = Console.ReadLine();
-        // }
-
         public bool CheckBookendPunctuation()
         {
             if (Char.IsPunctuation(_targetWord[0]) || Char.IsPunctuation(_targetWord[_targetWord.Length - 1]))
@@ -161,179 +154,32 @@ namespace WordCounter
             for(int i = 0; i < _searchList.Count; i++)
             {
                 ReplaceWord(_searchList[i], _listIndex);
-                // IncrementListIndex();
-                // ResetCleanerValues();
+                IncrementListIndex();
+                ResetCleanerValues();
             }
+        }
+
+        public void IncrementMatches()
+        {
+            _matches++;
+        }
+
+        public void SetMatches()
+        {
+            CleanList();
+            foreach(string word in _searchList)
+            {
+                if(word == _targetWord)
+                {
+                    IncrementMatches();
+                }
+            }
+        }
+
+        public int Matches()
+        {
+            SetMatches();
+            return _matches;
         }
     }
 }
-
-        //Old Code
-//         private int _arrayStart; //possible edge case properties
-//         private int _arrayEnd; //possible edge case properties
-//         private int _arrayRange; //possible edge case properties
-//
-//         private char[] _searchArray; //possible edge case properties
-//         private string _cleanWord;
-//         private List<string> _cleanSearchList = new List<string>() { }; //possible edge case properties
-//
-//
-//         public string GetTargetWord()
-//         {
-//             return _targetWord;
-//         }
-//
-//         public void SetTargetWord(string targetWord)
-//         {
-//             _targetWord = targetWord.ToLower();
-//         }
-//
-//         public bool CheckTargetWordForPunctuation()
-//         {
-//             foreach(char letter in _targetWord)
-//             {
-//                 if(Char.IsPunctuation(letter))
-//                 {
-//                     return false;
-//                 }
-//             }
-//             return true;
-//         }
-//
-//         public void SetSearchPhrase(string searchPhrase)
-//         {
-//             _searchPhrase = searchPhrase;
-//         }
-//
-//         public string GetSearchPhrase()
-//         {
-//             return _searchPhrase;
-//         }
-//
-//         public void SetSearchPhraseToLower()
-//         {
-//             _searchPhrase = GetSearchPhrase().ToLower();
-//         }
-//
-//         public void SetSearchList(string searchPhrase)
-//         {
-//             SetSearchPhrase(searchPhrase);
-//             SetSearchPhraseToLower();
-//             _searchList = _searchPhrase.Split(' ').ToList();
-//         }
-//
-//         public List<string> GetSearchList()
-//         {
-//             return _searchList;
-//         }
-//
-//
-//         public void SetArrayEnd(string word)
-//         {
-//             _searchArray = word.ToCharArray();
-//             _arrayEnd = _searchArray.Length - 1;
-//         }
-//
-//         public int GetArrayEnd()
-//         {
-//             return _arrayEnd;
-//         }
-//
-//         //------------Buggy Edge Case Methods-----------------//
-//         //-----------Program Meets Minimum Requirements----//
-//
-//         // public void IncrementArrayStart()
-//         // {
-//         //     _arrayStart++;
-//         // }
-//         //
-//         // public int GetArrayStart()
-//         // {
-//         //     return _arrayStart;
-//         // }
-//         //
-//         // public void ReduceArrayEnd()
-//         // {
-//         //     _arrayEnd--;
-//         // }
-//         //
-//         // public void SetArrayRange()
-//         // {
-//         //     _arrayRange = (_arrayEnd - _arrayStart) + 1;
-//         // }
-//         //
-//         // public int GetArrayRange()
-//         // {
-//         //     return _arrayRange;
-//         // }
-//         //
-//         // public void ResetArrayCounters()
-//         // {
-//         //     _arrayStart = 0;
-//         //     _arrayEnd = 0;
-//         //     _arrayRange = 0;
-//         // }
-//         //
-//         // public char[] GetSearchArray()
-//         // {
-//         //     return _searchArray;
-//         // }
-//         //
-//         // public void SetCleanSearchList()
-//         // {
-//         //     foreach(string word in _searchList)
-//         //     {
-//         //         _searchArray = word.ToCharArray();
-//         //         SetArrayEnd();
-//         //         while(Char.IsPunctuation(_searchArray[_arrayStart]))
-//         //         {
-//         //             IncrementArrayStart();
-//         //         }
-//         //         while(Char.IsPunctuation(_searchArray[_arrayEnd]))
-//         //         {
-//         //             ReduceArrayEnd();
-//         //         }
-//         //         SetArrayRange();
-//         //         // _cleanWord = new String(_searchArray, _arrayStart, _arrayRange);
-//         //         _cleanSearchList.Add(new String(_searchArray, _arrayStart, _arrayRange));
-//         //         // ResetArrayCounters();
-//         //     }
-//         // }
-//         //
-//         // public List<string> GetCleanSearchList()
-//         // {
-//         //     return _cleanSearchList;
-//         // }
-//
-//         //------------End of Buggy Edge Case Methods-----------------//
-//
-//         public void IncrementMatches()
-//         {
-//             _matches++;
-//         }
-//
-//         public int GetMatches()
-//         {
-//             return _matches;
-//         }
-//
-//         public void SetMatches()
-//         {
-//             foreach(string testWord in _searchList)
-//             {
-//                 if (testWord == _targetWord)
-//                 {
-//                     IncrementMatches();
-//                 }
-//             }
-//         }
-//
-//         public int NumberOfMatches(string targetWord, string searchPhrase)
-//         {
-//             SetTargetWord(targetWord);
-//             SetSearchList(searchPhrase);
-//             SetMatches();
-//             return _matches;
-//         }
-//     }
-// }
