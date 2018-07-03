fb68305e2a7641b375861609f02af3116704954d
Ryan Murry <ryanjmurry@gmail.com>
2018-07-02 23:34:10 -0700

    begin to add MVC files and writing controller methods

--- WordCounter/Controllers/HomeController.cs
+++ WordCounter/Controllers/HomeController.cs
@@ -2,17 +2,16 @@ using Microsoft.AspNetCore.Mvc;
 using WordCounter.Models;
 using System.Collections.Generic;
 
-namespace CDLoop.Controllers
+namespace WordCounter.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            CD newCD01 = new CD("Tight Knit", "Vetiver", 10);
-            CD newCD02 = new CD("The Gaudy Side of Town", "Gayngs", 12);
-            CD newCD03 = new CD("Scorpion", "Drake", 15);
-            List<CD> cdList = new List<CD>() { newCD01, newCD02, newCD03 };
+            RepeatCounter newRepeatCounter = new RepeatCounter(Request.Query["target-word", Request.Query["search-phrase"]]);
             return View(cdList);
+            newRepeatCounter.Matches();
+            return View(newRepeatCounter);
         }
     }
 }
